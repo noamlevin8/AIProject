2296f6b54e31215a49b4df563bd6966bae3398cb
noamlevin8 <noamlevin11@gmail.com>
2024-06-10 22:33:26 +0300

    variable elimination

--- src/Factor.java
+++ src/Factor.java
@@ -1,10 +1,15 @@
 import java.util.ArrayList;
 
 public class Factor {
-    ArrayList<Variable> vars = new ArrayList<>();
+    ArrayList<String> vars = new ArrayList<>();
     String[][] table;
 
     public Factor(String[][] t){
+        this.table = t;
 
+        for(int j = 0; j < t[0].length-1; j++)
+        {
+            this.vars.add(t[0][j]);
+        }
     }
 }
--- src/VariableElimination.java
+++ src/VariableElimination.java
@@ -7,22 +7,45 @@ public class VariableElimination {
 
     }
 
-    public void deleteEvidence(String evidenceName, String value, Map<String, String[][]> cpt) {
+    public void deleteEvidence(Variable evidence, String value, Map<String, String[][]> cpt) {
         for (Map.Entry<String, String[][]> entry : cpt.entrySet())
         {
             for(int j = 0; j < entry.getValue()[0].length-2; j++)
             {
-                if(entry.getValue()[0][j].equals(evidenceName))
+                if(entry.getValue()[0][j].equals(evidence.name))
                 {
-//                    if(j == 0)
-//                    {
-//                        cpt.remove(entry.getKey());
-//                    }
-//                    else
-//                    {
-//
-//                    }
+                    String[][] newFactor = new String[entry.getValue().length / evidence.outcomes.size()][entry.getValue()[0].length - 1];
 
+                    int index1 = 1;
+                    int index2 = 0;
+
+                    // First row
+                    for(int k = 0; k < newFactor[0].length; k++)
+                    {
+                        if(!newFactor[0][k].equals(evidence.name))
+                            newFactor[0][k] = entry.getValue()[0][k];
+                        else
+                            k--;
+                    }
+
+                    for(int n = 1; n < entry.getValue().length; n++)
+                    {
+                        if(entry.getValue()[n][j].equals(value))
+                        {
+                            for(int m = 0; m < entry.getValue()[0].length; m++)
+                            {
+                                if(m != j)
+                                {
+                                    newFactor[index1][index2] = entry.getValue()[n][m];
+                                    index2++;
+                                }
+                            }
+                            index1++;
+                            index2 = 0;
+                        }
+                    }
+                    Factor f = new Factor(newFactor);
+                    VariableElimination.factors.add(f);
                 }
             }
         }
